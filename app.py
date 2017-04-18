e8fb596f4cd575133332e0852c5f0f166387a942
Denis Moskovchenko <moskovchenkody@gmail.com>
2017-04-19 00:34:15 +0300

    исправление ошибок

--- app.py
+++ app.py
@@ -55,17 +55,24 @@ class BarrelEntity(Entity):
 class World(object):
     """Описание игрового мира"""
 
+    WIDTH = 23
+    HEIGHT = 21
+
     def __init__(self):
         """ Определяем список объектов доступных для класса """
         self.ships = []
         self.barrels = []
         self.enemyships = []
 
+        self.field = []
+
     def update(self):
 
         self.ships = []
         self.barrels = []
         self.enemyships = []
+        # инициализируем поле
+        self.field = [0] * World.WIDTH * World.HEIGHT
 
         my_ship_count = int(raw_input())  # the number of remaining ships
         entity_count = int(raw_input())  # the number of entities (e.g. ships, mines or cannonballs)
@@ -74,12 +81,15 @@ class World(object):
             entity_id, entity_type, x, y, arg_1, arg_2, arg_3, arg_4 = raw_input().split()
             if entity_type == EntityType.BARREL:
                 self.barrels.append(BarrelEntity(int(entity_id), int(x), int(y), int(arg_1)))
+                self.field[int(x)+ int(y) * World.HEIGHT] = 1
             elif entity_type == EntityType.SHIP:
                 ship = ShipEntity(int(entity_id), int(x), int(y), int(arg_1), int(arg_2), int(arg_3), int(arg_4))
                 if arg_4 == "1":
                     self.ships.append(ship)
                 else:
                     self.enemyships.append(ship)
+
+                self.field[int(x)+ int(y) * World.HEIGHT] = 1
 
 class Commands(object):
     MOVE = 1
@@ -94,23 +104,28 @@ class Actions(object):
     NEAR_ENEMY = 3
     NEED_RUM = 4
     MOVE_ENEMY = 5
-    MOVE_RUM = 5
+    MOVE_RUM = 6
 
 class Problems(object):
     MOVE = 1
 
+def SUB(l, exclude = None):
+    if not exclude is None:
+        return [x for x in l if x!=exclude]
+    return l
+    
 class Strategy(object):
 
     def __init__(self, world):
         self.world = world
 
-    def get_actions(self, ship):
+    def get_actions(self, ship, exclude = None):
 
         command = None
         action = None
 
-        near_enemy = ship.find_near_entity(self.world.enemyships)
-        near_barrel = ship.find_near_entity(self.world.barrels)
+        near_enemy = ship.find_near_entity(SUB(self.world.enemyships, exclude))
+        near_barrel = ship.find_near_entity(SUB(self.world.barrels, exclude))
 
         while command is None:
             if action is None:
@@ -118,36 +133,33 @@ class Strategy(object):
 
             elif action == Actions.MOVE:
                 
-                print >> sys.stderr, "ENEMY" ,ship.dist_to(near_enemy) < 4, ship.dist_to(near_enemy)
                 
                 if near_enemy is not None and ship.dist_to(near_enemy) < 4:
+                    print >> sys.stderr, "ENEMY" ,ship.dist_to(near_enemy) < 4, ship.dist_to(near_enemy)
                     action = Actions.FIRE
                     
-                elif ship.rum > 60:
+                elif ship.rum > 60 and near_enemy is not near_enemy:
                     action = Actions.MOVE_ENEMY
                 else:
                     action = Actions.NEED_RUM
             elif action == Actions.MOVE_ENEMY:
-
-                target = near_enemy
-                if ship.dist_to(target) > 3:
-                    command = (Commands.MOVE, target)
+                if ship.dist_to(near_enemy) > 3:
+                    command = (Commands.MOVE, near_enemy)
                 else:
                     action = Actions.FIRE
 
             elif action == Actions.NEED_RUM:
-                target = near_barrel
-                if target is None:
+                if near_barrel is None:
                     action = Actions.MOVE_ENEMY
                 else:
-                    command = (Commands.MOVE, target)
+                    command = (Commands.MOVE, near_barrel)
 
             elif action == Actions.FIRE:
                 command = (Commands.FIRE, near_enemy)
             print >> sys.stderr, action, command
 
 
-        return self.parse_command(command)
+        return command
 
     def parse_command(self, command):
         if command[0] == Commands.MOVE:
@@ -166,7 +178,20 @@ while True:
 
     STRATEGY = Strategy(WORLD)
 
-    for my_ship in WORLD.ships:
-        result = STRATEGY.get_actions(my_ship)
-        print result
+    commands = []
 
+    for my_ship in WORLD.ships:
+        commands.append(STRATEGY.get_actions(my_ship))
+
+    # проверяем есть ли дубли, если есть тогда исключаем общую цель
+    com_len = len(commands)
+    for i in xrange(com_len - 1):
+        for j in xrange(i, com_len):
+            if commands[i][0] == Commands.MOVE and isinstance(commands[i][1],BarrelEntity):
+                if commands[i][1] == commands[j][1]:
+                    commands[j] = STRATEGY.get_actions(my_ship, commands[j][1])
+    
+    # выводим список доступных комманд
+    for comm in commands:
+        print STRATEGY.parse_command(comm)
+                    
